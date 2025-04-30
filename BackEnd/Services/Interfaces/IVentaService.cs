d6826d12cfe0956ee290fe486881982b22e361d3
Nahumms0320 <nahum.mora.soto@est.una.ac.cr>
2025-04-30 17:56:43 -0600

    BackEnd Venta Completo

---
+++ BackEnd/Services/Interfaces/IVentaService.cs
@@ -0,0 +1,14 @@
+﻿using BackEnd.DTO;
+
+namespace BackEnd.Services.Interfaces
+{
+    public interface IVentaService
+    {
+
+        List<VentaDTO> GetVentas();
+        VentaDTO GetVentaById(int id);
+        VentaDTO AddVenta(VentaDTO parametro);
+        VentaDTO UpdateVenta(VentaDTO parametro);
+        VentaDTO DeleteVenta(int id);
+    }
+}
